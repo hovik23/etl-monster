b01008de776503b3a2ff265618ec7816e84b2de1
Hovhannes <hovhannes@hovhannes-2.local>
2022-09-02 19:48:43 +0400

    added login form base

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar.js';
+import SignUpForm from './components/SignUpForm.jsx'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <SignUpForm/>
     </div>
   );
 }
