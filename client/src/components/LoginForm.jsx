8a338524692aa748f8b3f6ef67595b374affc099
Hovhannes <hovhannes@hovhannes-2.local>
2022-09-04 13:16:53 +0400

    added sign-up forms

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,7 +1,19 @@
 function LoginForm() {
 	return(
-		<>
-		</>
+		<form>
+			<div class="form-group row">
+			    <label for="staticEmail" class="col-sm-2 col-form-label">Login</label>
+			    <div class="col-sm-10">
+			      <input type="text" class="form-control" id="inputEmail" placeholder="email@example.com"/>
+			    </div>
+			</div>
+			<div class="form-group row">
+			    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+			    <div class="col-sm-10">
+			      <input type="password" class="form-control" id="inputPassword" placeholder="Password"/>
+			    </div>
+			</div>
+		</form>
 	)
 }
 
--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -1,30 +1,7 @@
 function SignUpForm() {
 	return(
 		<form>
-			<div class="form-group">
-				<div class="form-row">
-				    <div class="form-group col-md-6">
-				      <label for="inputEmail4">Email</label>
-				      <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
-				    </div>
-				    <div class="form-group col-md-6">
-				      <label for="inputPassword4">Password</label>
-				      <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
-				    </div>
-				  </div>
-				<label for="exampleInputEmail1">Email address</label>
-				<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-				<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-				</div>
-				<div class="form-group">
-				<label for="exampleInputPassword1">Password</label>
-				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-				</div>
-				<div class="form-check">
-				<input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-				<label class="form-check-label" for="exampleCheck1">Check me out</label>
-			</div>
-			<button type="submit" class="btn btn-primary">Submit</button>
+		
 		</form>
 	)
 }
